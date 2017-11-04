c91ba97ee4b4cb09c4532736d49e60db67c60dab
tak-kun <tak-kun@github.com>
2017-11-05 01:07:25 +0700

    update bot.py for wip

--- bot.py
+++ bot.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
+import sys
 from telebot import types
 from datetime import datetime
 
 
 bot = telebot.TeleBot(config.token)
+online_flag = True
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+    # bot.send_message(message.chat.id, message.text)
+    # print(message.text)
+    # str_time = datetime.now().strftime('%H:%M:%S')
+    # bot.send_message(message.chat.id, str_time)
+    # bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'.format(name=message.text))
+    # keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # keyboard.add(*[types.KeyboardButton(name) for name in ['petuch', 'pidor']])
+    # bot.send_message(message.chat.id, 'ТЫ КТО?',
+    #     reply_markup=keyboard)
+
+    #bot.send_message(message.chat.id, message.text)
     print(message.text)
-    str_time = datetime.now().strftime('%H:%M:%S')
-    bot.send_message(message.chat.id, str_time)
-    bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'.format(name=message.text))
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(*[types.KeyboardButton(name) for name in ['petuch', 'pidor']])
-    bot.send_message(message.chat.id, 'ТЫ КТО?',
-        reply_markup=keyboard)
+    if message.text[0] == "/":
+        print('command enabled')
+
 
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+     bot.polling(none_stop=online_flag)
